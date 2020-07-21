129914845ad441dc83f725365c3f95e1a478d9e2
Suhrid Satyal <suhrid@email.com>
2020-07-21 14:52:51 +1000

    Build a playable madlibs program with spacy

--- madlibs/play.py
+++ madlibs/play.py
@@ -4,6 +4,16 @@ import spacy
 from dataclasses import dataclass
 from collections import defaultdict
 from typing import List, Any
+from prompt_toolkit import prompt
+from prompt_toolkit import print_formatted_text, HTML 
+from prompt_toolkit.styles import Style
+
+style = Style.from_dict({
+    'prompt': 'blue',
+    'blank': '#ff0066',
+    'answer': '#44ff00 italic',
+    'actual': 'purple',
+})
 
 MAX_FETCH_ATTEMPTS = 5
 MIN_WORDS = 5
@@ -32,7 +42,8 @@ def extract_wikipedia_sentence(
         Maximum number of retires to be made if sentence of min_words
         is not found
     """
-    random_page = wikipedia.page("Special:Random")
+    random_page_title = wikipedia.random(1)
+    random_page = wikipedia.page(random_page_title)
     sentences = random_page.content.split(".")
     result = []
     loop_count = 0
@@ -89,3 +100,62 @@ def mark_blanks(
         else:
             del pos_map[k]
     return mad_libs_tokens
+
+def tokens_to_string(
+    tokens: List[MadLibsToken]
+):
+    s = []
+    for t in tokens:
+        if t.is_visible:
+            s.append(t.text)
+        else:
+            blank = '_' * len(t.text)
+            s.append(f'<blank>{blank}</blank>')
+    return s
+
+def get_first_blank_index(tokens):
+    for i, t in enumerate(tokens):
+        if t.is_visible == False:
+            return i
+
+def main():
+    nlp_model = spacy.load("en_core_web_sm")
+
+    print_formatted_text(HTML(
+        "<prompt> Creating a madlib sentence ... </prompt>"), 
+        style=style)
+    sentence = extract_wikipedia_sentence(MIN_WORDS, MAX_FETCH_ATTEMPTS)
+    madlib_tokens = mark_blanks(*tokenize(sentence, nlp_model))
+    blank_indices = [i for i, t in enumerate(madlib_tokens) if not t.is_visible] 
+
+    s = tokens_to_string(madlib_tokens) 
+    actual = s.copy()
+
+    mark = get_first_blank_index(madlib_tokens)
+
+    print_formatted_text(HTML(" ".join(s[:mark+1])), style=style)
+
+    for i, blank_index in enumerate(blank_indices):
+        text = prompt(HTML(
+            "<prompt> Enter blank word: </prompt>"), 
+            style=style)
+
+        madlib_tokens[blank_index].is_visible = True
+        s[blank_index] = f'<answer>{text}</answer>'
+        actual[blank_index] = f'<actual>{madlib_tokens[blank_index].text}</actual>'
+
+        if i < len(blank_indices) - 1:
+            print()
+            mark = get_first_blank_index(madlib_tokens)
+            print_formatted_text(HTML(" ".join(s[:mark + 1])), style=style)
+            print()
+    print()
+    print_formatted_text(HTML("<prompt> You said: </prompt>"), style=style)
+    print_formatted_text(HTML(" ".join(s)), style=style)
+    print()
+    print_formatted_text(HTML("<prompt> Actual sentence: </prompt>"), style=style)
+    print_formatted_text(HTML(" ".join(actual)), style=style)
+
+
+if __name__ == '__main__':
+    main()
